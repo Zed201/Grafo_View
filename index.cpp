f4fc1e45c9e3bc4502dc3ea2ef3426ac1bbe0039
Luiz Gustavo <luizg0509@gmail.com>
2024-02-03 00:02:06 -0300

    comentarios e testes

--- index.cpp
+++ index.cpp
@@ -1,14 +1,23 @@
 // so preguica de criar o .h
 #include "./GMatriz.cpp"
+// basicamente nesse cpp descrevemos o grafo e a operacao
+// eles elasa Tranverse de DFS e o de BSf, alem do print normal, e dps dijkstra
 
 int main(int argv, char** argc){
-        GrafoMatriz grafu({"a", "b", "c", "d"});
-        grafu.Add("a", {{"b", 2}, {"d", 3} });
-        grafu.Add("b", {{"c", 5}, {"d", 1} });
-        grafu.Add("c", {{"d", 3}});
-        grafu.Add("d", {{"c", 6} });
+        GrafoMatriz grafu({"6", "1", "2", "3", "4", "5", "0", "7", "8", "9"});
+        grafu.Add("0", { });
+        grafu.Add("1", {{"0", 1} });
+        grafu.Add("2", {{"1", 1}, {"3", 1}, {"4", 1} });
+        grafu.Add("3", {{"1", 1} });
+        grafu.Add("4", {{"3", 1} });
+        grafu.Add("5", {{"4", 1} });
+        grafu.Add("6", {{"5", 1}, {"2", 1}, {"7", 1} });
+        grafu.Add("7", {{"8", 1}, {"9", 1} });
+        grafu.Add("8", { });
+        grafu.Add("9", { });
+        // so compilar e passar como argumento o nome do file final, o python vai cuidar de tudo
         printf("%s\n", argc[1]);
-        grafu.TranverseBFS();
+        grafu.TranverseDFS();
         
         return 0;
 }
--- index.py
+++ index.py
@@ -1,13 +1,11 @@
 # graficos salvando onde deve
-# fazer funcs para compor algumas funcoes, melhorar as cores e algumas coisas como o with
-# Copiar o CMatrix e dar um jeito de printar em file.txt 
 # Tentar criar algum gif com o python e os estagios de cada busca
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
 
-# coisas para cores
+# coisas para cores, para selecionar cores de destaque nos nodos e nos lables
 rd.seed()
 cores_nodes = ['red', 'blue', 'black', '#0D1321', '#FE5F55', '#8A1C7C', 'black']
 cores_text = ['black', 'whitesmoke', 'whitesmoke', '#F7F7FF', 'black', 'whitesmoke', 'white']
@@ -15,10 +13,13 @@ ind_cor = rd.randint(0, len(cores_nodes) - 1)
 
 # Funciona para ler um file.txt e retorna a matriz de numpy e o dict com os nomes e os "index's"
 def leitura(file_name):
+    # cria um dict que vai ser retornado e serve para adiiconar os labels ao grafo
     Nomes_index = dict()
     with open("./{0}".format(file_name)) as file:
+        # o nome final do arquivo
         resu_fina = str(file.readline()).strip()
         vertices_td = int(file.readline())
+        # leitura tanto dos nomes do nos e depois da matriz, np array no caso, respetivamente
         for i in range(vertices_td):
             node_tmp = str(file.readline()).strip()
             Nomes_index[i] = node_tmp
@@ -29,30 +30,41 @@ def leitura(file_name):
                 Matriz[i][ind] = int(el)
     return (Matriz, Nomes_index, resu_fina)    
 
+# funcao que passa o nome apenas do arquivo png final, a matriz de adjascencia e o dict dos nomes dos grafos
 def Draw(img_name, Matriz, nomes_grafo):
+
     # o Grafo é escolhido na linha de baixo, podemos usar:
     # nx.Graph para nao direcionado ponderados
     # nx.DiGraph para ponderados e direcionados
     # nx.MultiGraph para multigrafos nao direcionados (nao funciona direito)
     # nx.MultiDigraph, para multigrafos direcionados(nao funciona direito, pois as multiplas arestas parecem nao funcionar com matrizes)
+    # cria o grafo em si, e passa o dict como os labels deles
     Grafo = nx.relabel_nodes(nx.DiGraph(Matriz), nomes_grafo, 'name')
     plt.figure(1)
+    # pega os atributos name e pesos
     pesos = nx.get_edge_attributes(Grafo, "weight")
     nomes = nx.get_node_attributes(Grafo, "name")
+    # adiciona tamanaho aos nodos a depender dos labels deles
     nodos_size = [len(label) * 400 for label in nomes_grafo.values()]
+
+    # DEBUG:
     #print(nomes)
     #print(pesos)
+
+    # tipos de layouts que dizem respetto a distribuição dos nodos
     posi = nx.spring_layout(Grafo)             # modo spring, de mola
     #posi = nx.circular_layout(Grafo)     # modo circular
     #posi = nx.planar_layout(Grafo)     # forma planar, sem os edges se cuzarem
     #posi = nx.shell_layout(Grafo)    # n sei oque é shell
 
+    # draw ´principal no plot
     nx.draw(Grafo, pos=posi, node_size=nodos_size, node_color=cores_nodes[ind_cor])
-
+    # draw dos nomes do nodos
     nx.draw_networkx_labels(Grafo, pos=posi, font_size=10, font_color=cores_text[ind_cor])
-
+    # draw de cada peso de aresta 
     for edge, w in pesos.items():
         nx.draw_networkx_edge_labels(Grafo, pos=posi, edge_labels={(edge[0], edge[1]) : w})
+    # salvamento da imagem
     plt.savefig("./{0}.png".format(img_name), format='png')
 
 matrix, nomes_nds, final_path = leitura("index.txt")
